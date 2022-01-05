2e921624cf7b06228faaa8fa71e799723206ace5
Neil Sweeney <neil@wolfiezero.com>
2022-01-05 20:36:53 +0000

    Remove prettier sub-modules from TypeScript

--- typescript/prettier.js
+++ typescript/prettier.js
@@ -2,7 +2,7 @@ const { rules } = require('../shared/prettier');
 
 module.exports = {
   plugins: ['@typescript-eslint', 'unicorn', 'prettier'],
-  extends: ['prettier', 'prettier/@typescript-eslint', 'prettier/unicorn'],
+  extends: ['prettier'],
   rules: {
     ...rules,
   },
